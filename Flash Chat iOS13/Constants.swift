0e469783737492dda907812504e791e79992bf15
Hnat Danylevych <uahnat@gmail.com>
2021-03-09 10:00:04 +0200

    init commit

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,29 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Mac on 07.03.2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+struct K {
+    static let appName = "⚡️Flash Chat"
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Mac on 07.03.2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String // email of sender: 1@2.com
+    let body: String
+}
